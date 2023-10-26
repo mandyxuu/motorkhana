06217003254c50198434ada3c8da6f54197d37ea
Mandy Xu <140071724+mandyxuu@users.noreply.github.com>
2023-10-27 00:14:07 +1300

    update

--- README.md
+++ README.md
@@ -1,5 +1,40 @@
 # motorkhana
-local change test
-web change --test
+1. navigation page  9/10/2023 create url and url name in base.html
+2. list of courses add the course images 10/10
+insert the image path in courselist,            <td><img src="/static/{{ course.2}}" alt="" width='200' ></td>
 
-local change2
+3. list junior driver list 10/10
+        create juniordriver html junior driver name,age, caregiver from database to webpage
+        create new route use mysql to query the junior driver info. 
+        from driver table join caregiver table.
+                {% for driver in junior_driver %} 'junior_driver'juniorList in return clause
+4. list of driver
+
+            <tr {% if driver.2 == 1 %} class="table-warning"{% endif %}>
+highlight the junior driver in the list,create the dervier name clickable
+5. driver's run details
+@app.route("/Rundetails", methods = ['GET']) connect to the list of driver app route.
+in driverlist.html    <td><a href="Rundetails?driverName={{driver.1}}">{{driver.1}}</a></td>
+so when click in the drivername tehen display the driver's run details.
+6.overall reresults i think run_total,
+the headers are driver_id,driver_name (J if exist),car model, sum of 6 courses time (cup/prize)
+then show each driver's 6 course times.
+create two sql query in one route, and marry to one html
+show all the overall results in the same query
+
+assumption: share driver list from junior list to driver list, then to run details then overall results.
+
+reuse the driver list in public and admin webapge
+
+in pubilc webpage: click to driver list, use driver drop down, to see the specific driver name, and run details and overall results buttons on the top.saving route?
+
+when i created the pulldown of driver list, when i failed to selct a driver, every time would jump to <select a driver>
+
+driver[0]|string == driver_id
+in edit run, do I need insert to run table after editing??
+dont need to, 
+
+
+
+    
+
--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import url_for
+
 import re
 from datetime import datetime
 import mysql.connector
@@ -24,16 +25,12 @@ def getCursor():
     return dbconn
 
 @app.route("/")
-def home():
+def base():
     return render_template("base.html")
 
-@app.route("/listdrivers")
-def listdrivers():
-    connection = getCursor()
-    connection.execute("SELECT * FROM driver;")
-    driverList = connection.fetchall()
-    print(driverList)
-    return render_template("driverlist.html", driver_list = driverList)    
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
 
 @app.route("/listcourses")
 def listcourses():
@@ -42,12 +39,302 @@ def listcourses():
     courseList = connection.fetchall()
     return render_template("courselist.html", course_list = courseList)
 
+@app.route("/listdrivers")
+def listdrivers():
+    connection = getCursor()
+    sql = """ SELECT driver.driver_id
+            , concat(driver.first_name,' ',driver.surname) AS driverName
+            , car.model
+            , car.drive_class
+            , if (driver.age <= 25,1,0) AS isJunior
+            FROM motorkhana.driver
+            JOIN motorkhana.car ON driver.car = car.car_num
+            LEFT JOIN motorkhana.driver as caregiver on driver.caregiver= caregiver.driver_id
+            ORDER BY driver.surname, driver.first_name
+            ;"""
+    connection.execute(sql)
+    driverList = connection.fetchall()
+    print(driverList)
+   
+    return render_template("driverlist.html", driver_list = driverList)
+
+@app.route("/listdrivers/filter", methods = ['GET','POST'])
+def listdriversfilter():
+    driver_id = request.form.get('driver')
+    driverID = request.args.get('driverid')
+    connection = getCursor()
+    sql1 = """SELECT driver.driver_id
+        , concat(driver.first_name,' ',driver.surname) AS driver_name
+        , car.model
+        , car.drive_class
+        , course.name AS course_name
+        , run.run_num 
+        , run.seconds
+        , run.cones
+        , if(run.wd =1, "WD","") AS wd
+        , ifnull(round((run.seconds + ifnull(run.cones,0) *5 + run.wd * 10),2),'dnf')AS run_total
+            FROM motorkhana.driver
+            JOIN motorkhana.car on driver.car = car.car_num
+            LEFT JOIN motorkhana.run ON run.dr_id = driver.driver_id
+            JOIN motorkhana.course ON course.course_id = run.crs_id
+            """
+    if request.method =='POST':
+        sql2 = "WHERE driver.driver_id= %s"
+        parameters = (driver_id,)
+    elif request.method == "GET":
+        sql2 = "WHERE driver.driver_id= %s"
+        parameters = (driverID,)
+    else:
+        sql2 = ""
+        parameters = ()
+    sql3 = "order by driver.driver_id ;"
+    sql= sql1 + " " +sql2+" " + sql3
+    connection.execute(sql,parameters)
+    runDetails = connection.fetchall()
+
+    connection = getCursor()
+    sql = """ SELECT driver.driver_id
+            , concat(driver.first_name,' ',driver.surname) AS driverName
+            FROM motorkhana.driver
+            LEFT JOIN motorkhana.driver as caregiver on driver.caregiver= caregiver.driver_id
+        ;"""  
+    connection.execute(sql)
+    dropList = connection.fetchall()
+    print(dropList)
+
+    return render_template("dropdriverlist.html", drop_list = dropList,run_details = runDetails,driver_id=driver_id,driverid = driverID)  
+  
+@app.route("/overall")
+def overall():
+    connection = getCursor()
+    sql = """SELECT
+            driver.driver_id,
+            IF(driver.age IS NOT NULL, CONCAT(driver.first_name, ' ', driver.surname, ' (J)'), CONCAT(driver.surname, ' ', driver.first_name)) AS driver_name,
+            MAX(car.model) AS model,
+            CAST(MIN(CASE WHEN course.course_id = 'A' THEN course_time ELSE NULL END )AS DECIMAL(6,2)) AS courseA,
+            CAST(MIN(CASE WHEN course.course_id = 'B' THEN course_time ELSE NULL END )AS DECIMAL(6,2)) AS courseB,
+            CAST(MIN(CASE WHEN course.course_id = 'C' THEN course_time ELSE NULL END )AS DECIMAL(6,2)) AS courseC,
+            CAST(MIN(CASE WHEN course.course_id = 'D' THEN course_time ELSE NULL END )AS DECIMAL(6,2)) AS courseD,
+            CAST(MIN(CASE WHEN course.course_id = 'E' THEN course_time ELSE NULL END )AS DECIMAL(6,2)) AS courseE,
+            CAST(MIN(CASE WHEN course.course_id = 'F' THEN course_time ELSE NULL END )AS DECIMAL(6,2)) AS courseF,
+            IF(COUNT(driver_id) = 6, CAST(SUM(course_time) AS DECIMAL(6 ,2)), 'NQ') AS overall_result,
+            
+            CASE WHEN RANK() OVER (ORDER BY (IF(COUNT(driver_id) = 6,SUM(course_time),9999) )) = 1 
+                    THEN 'cup'
+                WHEN RANK() OVER (ORDER BY  (
+                        IF(COUNT(driver_id) = 6,SUM(course_time),9999) )) BETWEEN 2 AND 5 THEN 'prize'
+                ELSE ''
+            END AS Award
+        FROM motorkhana.driver
+        JOIN motorkhana.car ON driver.car = car.car_num
+        LEFT JOIN (
+                SELECT dr_id, crs_id,
+                CASE
+                    WHEN MIN(run_total) IS NOT NULL THEN MIN(run_total)
+                    WHEN MIN(run_total) IS NULL AND MAX(run_total) IS NOT NULL THEN MAX(run_total)
+                    ELSE 'dnf'
+                END AS course_time
+                FROM (
+                    SELECT dr_id, crs_id, seconds + IFNULL(cones, 0) * 5 + wd * 10 AS run_total
+                    FROM motorkhana.run
+                        ) AS run_total
+                GROUP BY dr_id, crs_id
+                ) AS results ON results.dr_id = driver.driver_id
+        JOIN motorkhana.course ON course.course_id = results.crs_id
+        WHERE course_time <> 'dnf'
+        GROUP BY driver.driver_id, driver_name;"""
+    connection.execute(sql)
+    OverList = connection.fetchall()
+    return render_template("overall.html", over_all = OverList)
+
 @app.route("/graph")
 def showgraph():
     connection = getCursor()
-    # Insert code to get top 5 drivers overall, ordered by their final results.
+    sql = """SELECT driver.driver_id
+        ,if(driver.age is not null,concat(driver.first_name,' ',driver.surname,'(J)')
+        ,concat(driver.surname,' ',driver.first_name)) AS driver_name
+       , if( count(driver_id) = 6, cast(sum(overresults) as decimal(6,2)),'NQ') AS overall
+        FROM motorkhana.driver
+        JOIN motorkhana.car on driver.car = car.car_num
+        LEFT JOIN (select dr_id,
+				        crs_id,
+				        CASE WHEN MIN(run_total) is not null then min(run_total)
+					        WHEN min(run_total) is null and max(run_total) is not null then max(run_total)
+					        ELSE 'dnf'
+					        END AS overresults
+                    FROM (
+                            SELECT dr_id,
+                            crs_id,
+                            seconds + ifnull(cones,0) *5 + wd*10 as run_total 
+                            FROM motorkhana.run) as run_total
+                            GROUP BY dr_id,crs_id
+                        ) as reults
+        ON reults.dr_id = driver.driver_id
+        JOIN motorkhana.course ON course.course_id = reults.crs_id
+        WHERE overresults <> 'dnf'
+        GROUP BY driver.driver_id
+                ,driver_name
+        ORDER BY overall
+        LIMIT 5;"""
+    connection.execute(sql)
+    top5data = connection.fetchall()
+    bestDriverList = [f"{driver[0]} {driver[1]}" for driver in top5data]
+    resultsList = [driver[2] for driver in top5data]
     # Use that to construct 2 lists: bestDriverList containing the names, resultsList containing the final result values
     # Names should include their ID and a trailing space, eg '133 Oliver Ngatai '
 
     return render_template("top5graph.html", name_list = bestDriverList, value_list = resultsList)
 
+@app.route("/juniordrivers")
+def junior_drivers():
+    connection = getCursor()
+    sql = """SELECT driver.driver_id,driver.surname, driver.first_name,driver.age,CONCAT(caregiver.first_name ,' ' , caregiver.surname) AS Caregiver
+            FROM motorkhana.driver
+            JOIN motorkhana.CAR on driver.car = CAR.car_num
+            LEFT JOIN motorkhana.driver as caregiver on driver.caregiver= caregiver.driver_id
+            WHERE driver.age between 12 and 25
+            ORDER BY driver.age desc, driver.surname;"""
+    connection.execute(sql)
+    juniorList = connection.fetchall()
+    print(juniorList)
+    return render_template("juniordriver.html", junior_driver = juniorList)    
+
+@app.route("/driversearch", methods=["GET","POST"])
+def driversearch():
+    driverName = request.form.get('driver')
+    connection = getCursor()
+    sql=""" SELECT driver.driver_id
+            , concat(driver.first_name,' ',driver.surname) AS driverName
+            FROM motorkhana.driver
+            where concat(driver.surname,' ',driver.first_name) like %s
+            ;"""
+    parameters = (f'%{driverName}%',)
+    connection.execute(sql,parameters)
+    driverList = connection.fetchall()
+    return render_template("driversearch.html", driver_list= driverList,driver_name = driverName)
+
+@app.route("/editruns", methods = ["GET","POST"])
+def editruns():
+    connection = getCursor()
+    sql = "SELECT * FROM motorkhana.run;"
+    connection.execute(sql)
+    runData = connection.fetchall()
+    
+    if request.method == "POST":
+        driver_id = request.form.get("driver_id")
+        course_id =request.form.get("course_id")
+        run_num = request.form.get("run_num")
+        print(driver_id,course_id,run_num)
+        for run in runData:
+            print(type(run[3]))
+            if run[0]== int(driver_id)and run[1]== course_id and run[2]== int(run_num):
+                if run[3] == None:
+                    run_time = request.form.get("run_time")
+                    cones = request.form.get("cones")
+                    cones = 0 if cones == "" else cones
+                    wd = request.form.get("wd")
+                    wd = int(wd) if wd == '1' else 0
+                    print(cones)
+                    print(wd)
+                    try:
+                        run_time = float(run_time)
+                        if not(20<=run_time<=500):
+                            return ("Invaild 'run time' value, please input between 20 and 500.")
+                        
+                        cones = int(cones)
+                        if not(0 <= cones<= 25):
+                            return ("Invaild 'cones' number, please input between 0 and 25.")
+                        
+                        cur = getCursor()
+                        cur.execute("UPDATE motorkhana.run  SET seconds = %s, cones = %s, wd = %s WHERE dr_id=%s AND crs_id = %s AND run_num=%s;",(run_time, cones,wd ,driver_id,course_id,run_num))
+                        return "Add Driver run detail successfully"
+                    except ValueError :
+                        return ("Invaild data please input valid run time or cones.")
+        else:
+            run_time = request.form.get("run_time",run[2])
+            cones= request.form.get("cones",run[3])
+            wd = request.form.get("wd",run[4])
+            return( "Duplicate entries are not permitted. Please return to the run details and verify the information entered.")
+  
+    connection = getCursor()
+    sql=""" SELECT driver.driver_id
+            , concat(driver.first_name,' ',driver.surname) AS driverName
+            FROM motorkhana.driver
+            ;"""
+    connection.execute(sql)
+    driverList = connection.fetchall()
+
+    connection = getCursor()
+    sql = """SELECT *  FROM motorkhana.course ;"""
+    connection.execute(sql)
+    courseList = connection.fetchall()   
+    return render_template("editruns.html",driver_list = driverList,course_list = courseList)
+
+@app.route("/adddriver",methods =['GET','POST'])
+def adddrivers():
+    today = datetime.now()
+
+    if request.method=="POST":
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        car = request.form.get("car")
+        date_of_birth = request.form.get("date_of_birth")
+        caregiver_id = request.form.get("caregiver_id")
+        birthdate = None
+        age = None
+        caregiver_id = None
+
+        try:          
+            if date_of_birth:
+                birthdate = datetime.strptime(date_of_birth,"%Y-%m-%d")
+                age = today.year - int(birthdate.year) - ((today.month, today.day)<(int(birthdate.month),int(birthdate.day)))
+                if 12<= age <=25:
+                    age = age
+                    birthdate= birthdate
+                    if 12<= age<=16:
+                        caregiver_id = request.form.get("caregiver_id")
+                        if not caregiver_id:
+                            return "Junior drivers aged 16 or younger must have a designed caregiver"                    
+            else:
+                birthdate = None
+                age= None
+                caregiver_id = None
+        except ValueError:
+            return "Invalid date of birth or wrong date format,please follow 'yyyy-mm-dd'"
+
+        connection = getCursor()
+        sql = """INSERT INTO  motorkhana.driver(first_name,surname,date_of_birth,age,caregiver,car)\
+                VALUES(%s,%s,%s,%s,%s,%s);"""
+        connection.execute (sql,(first_name,last_name,birthdate,age,caregiver_id,car))
+       
+        driver_id = connection.lastrowid
+        print(driver_id)
+        
+        connection = getCursor()
+        sql = """SELECT *
+            FROM motorkhana.course"""
+        connection.execute(sql)
+        courseList = connection.fetchall()
+        for course in courseList:
+            for run_num in [1,2]:
+                sql = """INSERT INTO motorkhana.run (dr_id,crs_id,run_num,seconds,cones,wd) \
+                        VALUES(%s,%s,%s,null,null,0);"""
+                connection.execute(sql,(driver_id,course[0],run_num))
+        return "Driver added successfully."
+
+    connection = getCursor()
+    sql = """SELECT *
+            FROM motorkhana.car"""
+    connection.execute(sql)
+    carList = connection.fetchall()
+
+    connection = getCursor()
+    sql=""" SELECT driver.driver_id
+            , concat(driver.first_name,' ',driver.surname) AS driverName
+            FROM motorkhana.driver
+            WHERE driver.age is null
+            ;"""
+    connection.execute(sql)
+    caregiverList = connection.fetchall()
+
+    return render_template("adddrivers.html",caregiver_list = caregiverList,car_list = carList,today = today)
